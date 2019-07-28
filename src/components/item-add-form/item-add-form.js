7bda6a05054c3bc430247f3e263f684edfc44b7f
aydys <40449724+aydys@users.noreply.github.com>
2019-07-28 09:35:03 +0700

    работа с формами и конролируемый элемент

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,16 +3,39 @@ import React, {Component} from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            label: ''
+        };
+        
+        this.onLabelChange = (e) => {
+            this.setState({
+                label: e.target.value
+            });
+        };
+        this.onSubmit = (e) => {
+            e.preventDefault();
+            this.props.onItemAdded(this.state.label);
+            this.setState({
+                label: ''
+            });
+        }
+    }
       
     render() {
-        const { onItemAdded } = this.props;
         return (
-            <div className='item-add-form'>
-                <button className='btn btn-outline-secondary'
-                        onClick={ () => onItemAdded('some text') }>
+            <form className='item-add-form d-flex'
+                    onSubmit={this.onSubmit}>
+                <input type='text'
+                        className='form-control'
+                        onChange={this.onLabelChange}
+                        placeholder="What needs to be done"
+                        value={this.state.label} />
+                <button className='btn btn-outline-secondary'>
                     Add Item
                 </button>
-            </div>
+            </form>
         );
     }
 };
