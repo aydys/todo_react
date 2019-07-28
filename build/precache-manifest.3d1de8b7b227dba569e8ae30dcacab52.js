8661229b7d8fd1a8efc605421343d1f1f1fbfadf
aydys <40449724+aydys@users.noreply.github.com>
2019-07-28 15:50:26 +0700

    production

---
+++ build/precache-manifest.3d1de8b7b227dba569e8ae30dcacab52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30da8e8ed013eb41d29739da7711c1a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0de98e40f65fb5783496",
+    "url": "/static/css/main.7903cd2d.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "0de98e40f65fb5783496",
+    "url": "/static/js/main.5e99f58b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
